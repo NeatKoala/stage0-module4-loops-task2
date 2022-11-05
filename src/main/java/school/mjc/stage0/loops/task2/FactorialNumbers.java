c51e69033f57e2ab78d550035068491de6b46def
NeatKoala <49612688+NeatKoala@users.noreply.github.com>
2022-11-05 16:06:07 +0100

    Tasks done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int printFromInclusive = 0;
+        while (printFromInclusive <= printToInclusive) {
+            System.out.println(factorial(printFromInclusive));
+            printFromInclusive++;
+        }
+    }
 
+    public int factorial(int number) {
+        if (number == 0) {
+            return 1;
+        } else {
+            return (number * factorial(number - 1));
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,40 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+
     public void printPrimeNumbers(int printToInclusive) {
+        int integerToCheckForPrime = 0;
+
+        while (integerToCheckForPrime < printToInclusive) {
+            if (isPrime(integerToCheckForPrime)) {
+                System.out.println(integerToCheckForPrime);
+            }
+            integerToCheckForPrime++;
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number == 2) {
+            return true;
+        }
+
+        if (number % 2 == 0) {
+            return false;
+        }
+
+        int divider = 3;
+
+        while (divider * divider <= number) {
+            if (number % divider == 0)
+                return false;
+
+            divider += 2;
+        }
+
+        return true;
     }
 }
